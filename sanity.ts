181b24cec41ba61fda8e39d8d13e8b8b7e8cb3c8
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-08-14 04:43:18 +0100

    fix props error

--- sanity.ts
+++ sanity.ts
@@ -1,13 +1,19 @@
 import { createClient, createCurrentUserHook } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
+import { Post } from "./typings";
+
 
 export const config = {
     dataset: process.env.NEXT_SANITY_PUBLIC_DATASET || "production",
-    projectId: process.env.NEXT_SANITY_PUBLIC_PROJECT_ID,
+    projectId: process.env.NEXT_SANITY_PUBLIC_PROJECT_ID || "projectId",
     apiVersion: "2021-03-25",
     useCon: process.env.NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
 
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export const urlForMainImage = (source: Post["mainImage"]) =>
+    createImageUrlBuilder(config).image(source);
+
+export const urlForAuthorImage = (source: string) =>
+    createImageUrlBuilder(config).image(source);
